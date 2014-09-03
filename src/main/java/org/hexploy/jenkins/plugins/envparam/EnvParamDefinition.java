fa46eba7a3fb712eccb1e52d285cd8fbf3eb8f67
Alex Avdeichik <hexploy@gmail.com>
2014-09-03 22:16:44 +0400

    initial code commit

---
+++ src/main/java/org/hexploy/jenkins/plugins/envparam/EnvParamDefinition.java
@@ -0,0 +1,105 @@
+package org.hexploy.jenkins.plugins.envparam;
+
+import hudson.Extension;
+import hudson.model.*;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: hexploy
+ * @date: 03.09.14
+ */
+public class EnvParamDefinition extends ParameterDefinition {
+    @DataBoundConstructor
+    public EnvParamDefinition() {
+        super("environment", "");
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest req, JSONObject jsonObject) {
+        String env = req.getParameter("environment");
+        return new EnvParamValue(env, (EnvParamDescriptor) getDescriptor());
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest req) {
+        String env = req.getParameter("environment");
+        return new EnvParamValue(env, (EnvParamDescriptor) getDescriptor());
+    }
+
+    @Extension
+    public static class EnvParamDescriptor extends ParameterDescriptor {
+        private List<Environment> environments = new ArrayList<Environment>();
+
+        public EnvParamDescriptor() {
+            load();
+        }
+
+        public EnvParamDescriptor(Class<? extends ParameterDefinition> klazz) {
+            super(klazz);
+            load();
+        }
+
+        public List<Environment> getEnvironments() {
+            return environments;
+        }
+
+        public void setEnvironments(List<Environment> environments) {
+            this.environments = environments;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Environment parameter";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+            System.out.println("EnvParamDefinition config JSON=" + json);
+            JSONArray environmentsJson = json.getJSONArray("environments");
+            List<Environment> environmentsNew = new ArrayList<Environment>();
+            for (int i = 0; i < environmentsJson.size(); i++) {
+                JSONObject jsonEnv = environmentsJson.getJSONObject(i);
+                environmentsNew.add((Environment) jsonEnv.toBean(Environment.class));
+            }
+            environments = environmentsNew;
+            save();
+            return true;
+        }
+
+        public static class Environment {
+            private String name;
+            private String properties;
+
+            public Environment() {
+            }
+
+            public Environment(String name, String properties) {
+                setName(name);
+                setProperties(properties);
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public String getProperties() {
+                return properties;
+            }
+
+            public void setProperties(String properties) {
+                this.properties = properties;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/org/hexploy/jenkins/plugins/envparam/EnvParamValue.java
@@ -0,0 +1,63 @@
+package org.hexploy.jenkins.plugins.envparam;
+
+import hudson.EnvVars;
+import hudson.model.AbstractBuild;
+import hudson.model.ParameterValue;
+import hudson.util.VariableResolver;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.hexploy.jenkins.plugins.envparam.EnvParamDefinition.EnvParamDescriptor;
+import static org.hexploy.jenkins.plugins.envparam.EnvParamDefinition.EnvParamDescriptor.Environment;
+
+/**
+ * @author: hexploy
+ * @date: 03.09.14
+ */
+public class EnvParamValue extends ParameterValue {
+    private static Map<String, String> data;
+
+    @DataBoundConstructor
+    public EnvParamValue(String envName, EnvParamDescriptor descriptor) {
+        super("environment", envName);
+        data = new HashMap<String, String>();
+        data.put("environment", envName);
+        Properties properties = searchPropertiesForEnvironment(envName, descriptor);
+        for (String name : properties.stringPropertyNames()) {
+            data.put(name, properties.getProperty(name));
+        }
+    }
+
+    @Override
+    public VariableResolver<String> createVariableResolver(AbstractBuild<?, ?> build) {
+        return new VariableResolver.ByMap<String>(data);
+    }
+
+    @Override
+    public void buildEnvVars(AbstractBuild<?, ?> build, EnvVars env) {
+        env.putAll(data);
+    }
+
+    private static Properties searchPropertiesForEnvironment(String envName, EnvParamDescriptor descriptor) {
+        Properties properties = new Properties();
+        try {
+            List<Environment> environments = descriptor.getEnvironments();
+            for (Environment environment : environments) {
+                if (environment.getName().equals(envName)) {
+                    properties.load(new StringReader(environment.getProperties()));
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            //never happened
+        }
+        return properties;
+    }
+}
+
